e4c4f6aa60533f4afe35eecd96fb54950c3afb81
dickeyxxx <jeff@dickeyxxx.com>
2014-04-13 19:11:18 -0700

    show version

--- src/inject/meta.controller.js
+++ src/inject/meta.controller.js
@@ -1,3 +1,6 @@
-up.controller('MetaController', function($scope) {
-    $scope.version = '1.0';
+up.controller('MetaController', function($scope, $http, $window) {
+  var github = $window.document.title;
+  $http.get('http://updeps.dev/api/v1/repositories/' + github).success(function(repo) {
+    $scope.version = repo.version;
+  });
 });
